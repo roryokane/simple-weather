e751e958ab43d12f7590cdea4132ddf1ea06ee21
Rory O’Kane <rory@roryokane.com>
2012-12-23 09:35:05 +0200

    initial commit
    I delayed committing until I moved sensitive data inside an ignored config file.

---
+++ README.txt
@@ -0,0 +1,3 @@
+This will be a small website that displays the weather. It will only display information I almost always find useful – the weather for yesterday, today, and tomorrow. It will not display the forecast for the weather farther in the future than that.
+
+To start, I’m trying to write a command-line tool that reads the appropriate weather information (with some hard-coded inputs) through an API and then prints out the information I want to the command line.
---
+++ lib/main.rb
@@ -0,0 +1,30 @@
+# encoding: utf-8
+
+require 'wunderground'
+require_relative 'config_loader'
+
+CONFIG = ConfigLoader.new
+
+# geolocate browser user
+location = CONFIG["test user"]["location"]
+
+# read weather using the API
+# docs:
+# https://github.com/wnadeau/wunderground_ruby
+# http://www.wunderground.com/weather/api/d/docs?d=data/index
+
+def get_api
+	api_key = CONFIG["Wunderground"]["api key"]
+	return Wunderground.new(api_key)
+end
+w_api = get_api
+
+conditions_info = w_api.conditions_for(location)
+current_temp = conditions_info['current_observation']['temp_c']
+
+wunderground_link = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=" + location
+
+puts "location: #{location}"
+puts "Today's Weather"
+puts "current temperature: #{current_temp} °C"
+puts "more info: #{wunderground_link}"
