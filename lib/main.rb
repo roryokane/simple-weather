efeed9a25568ba44e736760b8552ecd6fbbdec53
Rory O’Kane <rory@roryokane.com>
2012-12-23 10:09:16 +0200

    add current weather with icon

--- lib/main.rb
+++ lib/main.rb
@@ -21,10 +21,13 @@ w_api = get_api
 
 conditions_info = w_api.conditions_for(location)
 current_temp = conditions_info['current_observation']['temp_c']
+current_weather = conditions_info['current_observation']['weather']
+current_weather_icon = conditions_info['current_observation']['icon_url']
 
 wunderground_link = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=" + location
 
 puts "location: #{location}"
 puts "Today's Weather"
 puts "current temperature: #{current_temp} °C"
+puts "current weather: #{current_weather} (icon: #{current_weather_icon})"
 puts "more info: #{wunderground_link}"
