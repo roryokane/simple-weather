c2f330ea1ad8db6812009ebf0cdcac55772fe69f
Rory O’Kane <rory@roryokane.com>
2012-12-23 10:14:54 +0200

    fill in stubs of upcoming work
    plus a coding style change: add parens to parameter-less method call

--- lib/main.rb
+++ lib/main.rb
@@ -17,8 +17,9 @@ def get_api
 	api_key = CONFIG["Wunderground"]["api key"]
 	return Wunderground.new(api_key)
 end
-w_api = get_api
+w_api = get_api()
 
+# TODO make this return a hash of info
 conditions_info = w_api.conditions_for(location)
 current_temp = conditions_info['current_observation']['temp_c']
 current_weather = conditions_info['current_observation']['weather']
@@ -26,8 +27,19 @@ current_weather_icon = conditions_info['current_observation']['icon_url']
 
 wunderground_link = "http://www.wunderground.com/cgi-bin/findweather/getForecast?query=" + location
 
+# TODO make this a text template that takes a hash
 puts "location: #{location}"
-puts "Today's Weather"
+puts ""
+puts "Yesterday’s Weather"
+puts "?"
+puts ""
+puts "Today’s Weather"
 puts "current temperature: #{current_temp} °C"
 puts "current weather: #{current_weather} (icon: #{current_weather_icon})"
+puts "hourly upcoming temperatures: ?"
+puts "hourly upcoming weather: ?"
+puts ""
+puts "Tomorrow’s Weather"
+puts "?"
+puts ""
 puts "more info: #{wunderground_link}"
